d1a6e501b79ecd33bacc900691e6f29282a94b6f
WinterMinaya <0956335582@aitec.edu.ec>
2026-07-20 16:37:12 -0500

    feat: add inventory API structure and Prisma schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { CategoriesModule } from './categories/categories.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ProductsModule, CategoriesModule, InventoryModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
